209d288365a7aa36fdf6792d9f55d26b2692a8f3
Abdul Basit <abdul.basit@thedevden.co>
2018-11-30 15:41:18 +0500

    Label is added to VC

--- extra/ViewController.swift
+++ extra/ViewController.swift
@@ -10,9 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var Label: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
+        Label.text = "hello"
     }
 
 
